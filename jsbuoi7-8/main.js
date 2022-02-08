bf05465c1cfdad9f792e3b1d81992d2cc1699ce7
Sinh Pham <19522129@gm.uit.edu.vn>
2022-02-08 09:21:07 +0700

    a

--- jsbuoi7-8/main.js
+++ jsbuoi7-8/main.js
@@ -29,16 +29,48 @@ function timsonhonhat(arr) {
 function timsoduongnhonhat(arr) {
   let min = arr[0];
   for (var i = 1; i < arr.length; i++) {
-    if((min < 0 || min > arr[i]) && arr[i] > 0 ) {
+    if ((min < 0 || min > arr[i]) && arr[i] > 0) {
       min = arr[i];
+    }
   }
-  }
-  return min
+  return min;
 }
+
+function timsochancuoi(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    if (arr[i] % 2 == 0) {
+      return arr[i];
+    }
+  }
+}
+function sapxepmang(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      var tmp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = tmp;
+    }
+  }
+  return arr;
+}
+function timsonguyento(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (let j = 2; j < arr[i]; j++) {
+      if (arr[i] % j == 0) {
+        return -1;
+      } else return arr[i];
+      break;
+    }
+  }
+}
+
 function nhapmang() {
   var numBer = document.getElementById("txtNumber").value * 1;
-  var kq = document.querySelector("#ketqua");
   mang.push(numBer);
+}
+function xuatmang() {
+  nhapmang();
+  var kq = document.querySelector("#ketqua");
   var tongSoDuong = tongsoduong(mang);
   kq.innerHTML = "Xuất mảng: [ " + mang + " ]";
   kq.innerHTML += "<div></div>" + "Tổng số dương là: " + tongSoDuong;
@@ -48,4 +80,26 @@ function nhapmang() {
   kq.innerHTML += "<div></div>" + "Số nhỏ nhất là: " + sonhonhat;
   var soduongnhonhat = timsoduongnhonhat(mang);
   kq.innerHTML += "<div></div>" + "Số dương nhỏ nhất là: " + soduongnhonhat;
+  var sochancuoicung = timsochancuoi(mang);
+  if (sochancuoicung == undefined) {
+    sochancuoicung = -1;
+  }
+  kq.innerHTML += "<div></div>" + "Số chẵn cuối cùng là: " + sochancuoicung;
+  var sapxep = sapxepmang(mang);
+  kq.innerHTML += "<div></div>" + "Sắp xếp mảng: [ " + sapxep + " ]";
+  var songuyento = timsonguyento(mang);
+  if (songuyento == undefined) {
+    songuyento = -1;
+  }
+  kq.innerHTML += "<div></div>" + "Số nguyên tố đầu tiên: " + songuyento;
+}
+
+function doicho() {
+  var kq = document.querySelector("#ketqua2");
+  var Num1 = document.getElementById("txtNumber1").value * 1;
+  var Num2 = document.getElementById("txtNumber2").value * 1;
+  const tmp = mang[Num1];
+  mang[Num1] = mang[Num2];
+  mang[Num2] = tmp;
+  kq.innerHTML = "Xuất mảng sau khi đổi chỗ: [ " + mang + " ]";
 }
